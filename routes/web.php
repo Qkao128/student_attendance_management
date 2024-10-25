354ed16e7b389d7eeb8fc8784a34e87f3c2482ca
MingLi(QK) <tanming1218@gmail.com>
2024-10-25 15:37:33 +0800

    login middleware

--- routes/web.php
+++ routes/web.php
@@ -7,43 +7,45 @@ use App\Http\Controllers\CourseAdminController;
 use App\Http\Controllers\UserAdminController;
 
 Route::get('/', function () {
-    return redirect()->route('login.index');
+    return redirect()->route('login');
 })->name('index');
 
-Route::name('login.')->prefix('login')->group(function () {
-    Route::get('/', [AuthController::class, 'loginIndex'])->name('index');
-    Route::post('/', [AuthController::class, 'login'])->name('request');
+Route::name('login')->prefix('login')->group(function () {
+    Route::get('/', [AuthController::class, 'loginIndex']);
+    Route::post('/', [AuthController::class, 'login'])->name('.request');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard.index');
-})->name('dashboard')->middleware('can:teacher');
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard.index');
+    })->name('dashboard')->middleware('can:teacher');
 
+    Route::get('/dashboard/monitor', function () {
+        return view('monitor.dashboard.index');
+    })->name('dashboard.monitor')->middleware('can:monitor');
 
-Route::get('/dashboard/monitor', function () {
-    return view('monitor.dashboard.index');
-})->name('dashboard.monitor')->middleware('can:monitor');
+    // Logout route
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    // Admin routes
+    Route::name('user.')->prefix('user')->group(function () {
+        Route::get('select-search', [UserAdminController::class, 'selectOption'])->name('select_search');
+    });
 
-// admin
-Route::name('user.')->prefix('user')->group(function () {
-    Route::get('select-search', [UserAdminController::class, 'selectOption'])->name('select_search');
-});
+    Route::name('course.')->prefix('course')->group(function () {
+        Route::get('/', [CourseAdminController::class, 'index'])->name('index');
+        Route::post('/', [CourseAdminController::class, 'store'])->name('store');
+        Route::patch('{id}', [CourseAdminController::class, 'update'])->name('update');
+        Route::get('select-search', [CourseAdminController::class, 'selectOption'])->name('select_search');
+        Route::get('{id}', [CourseAdminController::class, 'show'])->name('show');
+        Route::delete('{id}', [CourseAdminController::class, 'destroy'])->name('destroy');
+    });
 
-Route::name('course.')->prefix('course')->group(function () {
-    Route::get('/', [CourseAdminController::class, 'index'])->name('index');
-    Route::post('/', [CourseAdminController::class, 'store'])->name('store');
-    Route::patch('{id}', [CourseAdminController::class, 'update'])->name('update');
-    Route::get('select-search', [CourseAdminController::class, 'selectOption'])->name('select_search');
-    Route::get('{id}', [CourseAdminController::class, 'show'])->name('show');
-    Route::delete('{id}', [CourseAdminController::class, 'destroy'])->name('destroy');
-});
-
-Route::name('class.')->prefix('class')->group(function () {
-    Route::get('/', [ClassAdminController::class, 'index'])->name('index');
-    Route::post('/', [ClassAdminController::class, 'store'])->name('store');
-    Route::patch('{id}', [ClassAdminController::class, 'update'])->name('update');
-    Route::get('{id}', [ClassAdminController::class, 'show'])->name('show');
-    Route::delete('{id}', [ClassAdminController::class, 'destroy'])->name('destroy');
+    Route::name('class.')->prefix('class')->group(function () {
+        Route::get('/', [ClassAdminController::class, 'index'])->name('index');
+        Route::post('/', [ClassAdminController::class, 'store'])->name('store');
+        Route::patch('{id}', [ClassAdminController::class, 'update'])->name('update');
+        Route::get('{id}', [ClassAdminController::class, 'show'])->name('show');
+        Route::delete('{id}', [ClassAdminController::class, 'destroy'])->name('destroy');
+    });
 });
