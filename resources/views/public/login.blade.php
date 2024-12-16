90725519971079156271a934ca7273bbee78afe0
LAPTOP-BE1J63IC\User <junlintan1227@gmail.com>
2024-12-16 10:50:52 +0800

    public login page 2

--- resources/views/public/login.blade.php
+++ resources/views/public/login.blade.php
@@ -11,13 +11,15 @@
             <div class="row p-0 m-0">
         <div class="big-con p-0 m-0 position-absolute top-50 start-50 translate-middle">
             <div class="row d-flex justify-content-center">
-                <div class="col-sm-0 col-md-3"></div>
-                <div class="con col-md-6 col-12 card rounded-4 rounded-sm-none rounded-md-4">
+                <div class="col-sm-0 col-md-2"></div>
+                <div class="con col-md-8 col-12 card rounded-4 rounded-sm-none rounded-md-4">
                     <div class="card-con row">
                     <div class="card-header bg-primary text-white d-flex flex-column justify-content-center align-items-center rounded-top-sm-none rounded-4 col-12 col-md-5 shadow">
+                        
                         <div class="auth-logo-container mb-3 position-md-absolute" style="max-width: 300px">
-                            <img class="w-100 d-md-block" src="{{ asset('img/LoginLogo.png') }}" id="auth-logo">
+                            <img class="w-100 mt-3 mb-3 d-md-block" src="{{ asset('img/LoginLogo.png') }}" id="auth-logo">
                         </div>
+                        <div class="bar"></div>
                         <h1 class="fw-bold fs-4 fs-md-3 justify-content-center text-left text-md-center 
                         ">Student Attendance Management</h1>
                     </div>
@@ -32,31 +34,36 @@
                             </div>
                         @endif
 
-                        <div>
+                        <div class="form-container">
                         <form id="form" method="POST" action="{{ route('login.request') }}">
                             @csrf
-                            <h1 class="fw-bold d-flex justify-content-center" style="">Login</h1>
-                            <div class="form-group mb-3">
-                                <label class="form-label" for="name">Name</label>
+                            <div class="form-text">
+                                <h1 class="fw-bold d-flex justify-content-center" style="">Login</h1>
+                            </div>
+                            <div class="bar3"></div>
+                            <div class="center-form mt-sm-1 mt-3">
+                            <div class="form-group mb-4 mb-md-5">
+                                <label class="form-label mb-sm-3 mb-md-4 ms-2" for="name">Name</label>
                                 <input type="text" id="name" class="form-control" placeholder="name" required name="name"
                                     value="{{ old('name') }}">
                             </div>
 
-                            <div class="form-group mb-3">
-                                <label class="form-label" for="password">Password</label>
+                            <div class="form-group mb-4 mb-md-5">
+                                <label class="form-label mb-sm-3 mb-md-4 ms-2" for="password">Password</label>
                                 <input type="password" id="password" class="form-control" placeholder="password" required
                                     name="password" value="{{ old('password') }}">
                             </div>
-
+                            <div class="bar2"></div>
                             <button type="submit" class="btn btn-primary btn-lg w-100 mt-3 fw-bold"
                                 style="box-shadow: 1px 5px rgb(221, 221, 221);">Login</button>
+                            </div>
                         </form>
                         </div>
                     </div>
                     </div>
                 </div>
 
-                <div class="col-sm-0 col-md-3"></div>
+                <div class="col-sm-0 col-md-2"></div>
             </div>
         </div>
 
