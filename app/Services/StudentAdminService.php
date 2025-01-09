537f0962d1722afe01f9074e706e7ae7a6c98d66
MingLi(QK) <tanming1218@gmail.com>
2025-01-09 11:29:43 +0800

    admin - datatable serve error fixed

--- app/Services/StudentAdminService.php
+++ app/Services/StudentAdminService.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use App\Repositories\ClassRepository;
-use App\Repositories\studentRepository;
+use App\Repositories\StudentRepository;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
